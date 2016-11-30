66d21e784b56db4fe3e8acd5eac9f9d6a32eb2ea
fantaros <fantasymon@gmail.com>
2016-11-30 17:15:08 +0800

    android & javascript

---
+++ arch/java/WhisperAndroid/app/src/main/java/io/github/fantaros/whisperandroid/WhisperActivity.java
@@ -0,0 +1,13 @@
+package io.github.fantaros.whisperandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhisperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whisper);
+    }
+}
--- arch/javascript/whisper.js
+++ arch/javascript/whisper.js
@@ -1,4 +1,55 @@
 //whisper - fantaros
-(function (){
-	
-})();
+window.Whisper = (function AppDomain($){
+    var WhisperTable = [
+        
+    ];
+    function ucs2ValueArray(str) {
+        var ucs2Array = [], len = str.length, i;
+        for(i = 0; i < len; ++i) {
+            ucs2Array.push(str.charCodeAt(i));
+        }
+        return ucs2Array;
+    }
+
+    function ucs2ArrayToByteArray(ucs2Array) {
+        var byteArray = [], len = ucs2Array.length, i, ucs2Code, usc2h, usc2l;
+        for(i = 0; i < len; ++i) {
+            ucs2Code = ucs2Array[i];
+            ucs2h = (ucs2Code & 0xff00) >> 8;
+            ucs2l = (ucs2Code & 0x00ff);
+            byteArray.push(ucs2h);
+            byteArray.push(ucs2l);
+        }
+        return byteArray;
+    }
+
+    function stringGetBytes(str) {
+        var ucs2Array = ucs2ValueArray(str);
+        return ucs2ArrayToByteArray(ucs2Array);
+    }
+
+	$.WhisperKey = function (password, keyLength) {
+        if(typeof password === "string") {
+            var pwd = password, keylen = keyLength, passwordBytes = stringGetBytes(password);
+            return {
+                getKey : function (index) {
+                    
+                },
+                getRing : function (index) {
+
+                }
+            };
+        }
+        return {
+            getKey : function (index) {
+                    
+            },
+            getRing : function (index) {
+
+            }
+        };
+    };
+    return $;
+})(window.Whisper || function WhisperHost(){
+
+});
